fb3c5f675cfd3042076cf3934e50bf8a0b5a1b07
Davide <davide.crestini94@gmail.com>
2018-06-24 14:33:58 +0100

    added express endpoint

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello world, Davide')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('App listening on port 3000!'))
