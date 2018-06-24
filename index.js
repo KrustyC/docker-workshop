de0eb2d1ef97dee6e4383b46846536529e2db858
Davide <davide.crestini94@gmail.com>
2018-06-24 15:24:16 +0100

    added ockerfile and babel

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Davide!'))
 
-app.listen(3000, () => console.log('App listening on port 3000!'))
+const PORT = 3000
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
