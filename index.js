0b416afff39ccd92dab7186ff28d1726a93a2056
Davide <davide.crestini94@gmail.com>
2018-06-24 16:59:17 +0100

    added circleci config file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Davide!'))
+app.get('/', (req, res) => res.send('Hello Docker!'))
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
